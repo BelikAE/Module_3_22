f1de515721c5b2c2cce01132d8f02dee53dc3cf8
BelikAE <BelikAE@HOME-PC>
2024-07-28 14:46:10 +1000

    Выдача

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,6 +10,64 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите размерность массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Func<object, int[]> func1 = new Func<object, int[]>(GetArray);
+            Task<int[]> task1 = new Task<int[]> (func1,n);
+
+            Func<Task<int[]>, int[]> func2 = new Func<Task<int[]>, int[]> (PrintArray);
+            Task<int[]> task2 = task1.ContinueWith(func2);
+
+            
+            Action<Task<int[]>> action2 = new Action<Task<int[]>>(GetSum);
+            Task task3 = task2.ContinueWith(action2);
+
+            
+            Action<Task<int[]>> action3 = new Action<Task<int[]>>(GetMax);
+            Task task4 = task2.ContinueWith(action3);
+
+            task1.Start();
+           
+            Console.ReadKey(); 
+        }
+
+        static int[] GetArray(object a)
+        {
+            int n = (int)a;
+            int[] array = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++) 
+            {
+                array[i] = random.Next(0, 100);            
+            } 
+            return array;
+        }
+
+        static void GetSum(Task<int[]> task)
+        {
+            int[] array = task.Result;
+            int sum = array.Sum();
+            Console.WriteLine($"\nСумма массива: {sum}");
+            
+        }
+
+        static void GetMax(Task<int[]> task)
+        {
+            int[] array = task.Result;
+            int max = array.Max();
+            Console.WriteLine($"Максимальное число: {max}");
+            
+        }
+
+        static int[] PrintArray(Task<int[]> task)
+        {
+            int[] array = task.Result;
+            for (int i = 0;i < array.Count();i++) 
+            {
+                Console.Write($"{array[i]} ");
+            }
+            return array;
         }
     }
 }
